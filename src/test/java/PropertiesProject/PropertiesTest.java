fecc7b83e58bef45bd91a9ce8d40e6a91f47b99e
fatosbudiyar <fatosbudiyar@gmail.com>
2018-07-10 22:42:39 -0400

    property test

---
+++ src/test/java/PropertiesProject/PropertiesTest.java
@@ -0,0 +1,53 @@
+package PropertiesProject;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PropertiesTest {
+
+//Create a maven project 
+//
+//
+//Create a properties file to store 
+//URL = http://automationpractice.com/index.php
+//User = waya@IOreak.net
+//Password = password
+//
+//inside your test use the utility class to access URL , USER , PASSWORD
+//
+//Create a test for login functionlity 
+//
+//// NAVIGATE TO THE PAGE 
+//// ENTER USER NAME AND PASSWORD
+//// VERIFY YOU ARE LOGGED IN SUCCESSFULLY 
+WebDriver driver;
+
+	@BeforeClass //runs once for all tests
+	public void setUp() {
+		System.out.println("Setting up WebDriver in BeforeClass...");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+	}
+	
+	@Test
+	public void logIn() {
+	driver.get(ConfigLoader1.getMyValue("url"));		
+	driver.findElement(By.xpath("//a[@class='login']")).click();
+    	driver.findElement(By.id("email")).sendKeys(ConfigLoader1.getMyValue("User"));
+    	driver.findElement(By.id("passwd")).sendKeys(ConfigLoader1.getMyValue("Password"));
+    	driver.findElement(By.id("SubmitLogin")).click();
+	}
+	
+	
+}
